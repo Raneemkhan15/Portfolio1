e21b7237b0a7363f0071f5a685abf7c2ef0421fe
Raneemkhan15 <raneemkhan06@gmail.com>
2024-07-14 02:56:16 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ setTimeout(function() {
     document.querySelector('.loader').style.display = 'none';
     document.getElementById('header').style.display = 'block';
 }, 3000);
+
+function showsidebar(){
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.style.display="flex";
+}
+
+function hidesidebar(){
+    const sidebar = document.querySelector("#sidebar");
+    sidebar.style.display="none";
+}
