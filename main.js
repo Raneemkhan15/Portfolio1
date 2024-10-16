df152b9d79fb07921970daac7f629efc7d76e676
Raneemkhan15 <raneemkhan06@gmail.com>
2024-10-16 20:48:18 +0500

    update file

--- main.js
+++ main.js
@@ -1,41 +1,78 @@
 //Loader
 function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("navbar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  
-setTimeout(function() {
-    document.querySelector(".loader").style.display = 'none';
-    document.querySelector(".header") .style.display = 'flex';
+}
+
+setTimeout(function () {
+  document.querySelector(".loader").style.display = 'none';
+  document.querySelector(".header").style.display = 'flex';
 }, 3000);
 
 //Navbar
-function showsidebar(){
-    const sidebar = document.querySelector("#sidebar");
-    sidebar.style.display="flex";
+function showsidebar() {
+  const sidebar = document.querySelector("#sidebar");
+  sidebar.style.display = "flex";
 }
 
-function hidesidebar(){
-    const sidebar = document.querySelector("#sidebar");
-    sidebar.style.display="none";
+function hidesidebar() {
+  const sidebar = document.querySelector("#sidebar");
+  sidebar.style.display = "none";
 }
+
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
+
 
 //CountUp
-$(".number").counterUp({time:3000});
+$(document).ready(function () {
+  // $(".number").counterUp({ time: 3000 });
 
-$(document).ready(function() {
-  $('button').on('click', function() {
-      const category = $(this).data('category');
 
-      if (category === 'all') {
-          $('.item').show();
-      } else {
-          $('.item').hide();
-          $('.item[data-category="' + category + '"]').show();
-      }
+
+  $('.filter').on('click', function () {
+    const category = $(this).data('category');
+
+    if (category === 'all') {
+      $('.item').show();
+    } else {
+      $('.item').hide();
+      $('.item[data-category="' + category + '"]').show();
+    }
   });
 });
